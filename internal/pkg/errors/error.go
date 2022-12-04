ec105d95d2ee81e25d50b8bbba1290cec44e05e5
xiaokang <648638730@qq.com>
2022-12-04 20:45:00 +0800

    修改

--- internal/pkg/errors/error.go
+++ internal/pkg/errors/error.go
@@ -1,8 +1,10 @@
 package ex
 
-import "github.com/lovechung/api-base/api/user"
+import (
+	"github.com/lovechung/api-base/api/user"
+)
 
 var (
-	UserNotFound = user.ErrorUserNotFound("该用户不存在")
-	UserIsFreeze = user.ErrorUserIsFreeze("该用户已冻结，请联系管理员")
+	UserNotFound = user.ErrorUserNotFound(user.UserServiceErrorReason_USER_IS_FREEZE.String() + "|该用户不存在")
+	UserIsFreeze = user.ErrorUserIsFreeze(user.UserServiceErrorReason_USER_IS_FREEZE.String() + "|该用户已冻结，请联系管理员")
 )
